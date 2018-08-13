79bad42eadb90da0aafbe67af513359a5d41a2f6
marco <spacesinmotion@gmail.com>
2018-08-13 12:06:46 +0200

    fix linux build
    using std::size_t

--- src/fileposition.h
+++ src/fileposition.h
@@ -1,11 +1,13 @@
 #ifndef FILEPOSITION_H
 #define FILEPOSITION_H
 
+#include <cstddef>
+
 struct FilePosition
 {
-  size_t pos{0};
-  size_t line{1};
-  size_t column{1};
+  std::size_t pos{0};
+  std::size_t line{1};
+  std::size_t column{1};
 };
 
 #endif  // FILEPOSITION_H
