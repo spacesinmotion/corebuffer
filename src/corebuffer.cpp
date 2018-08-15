202cc853ec11d647a5b8729d441712838aba1cd5
marco <spacesinmotion@gmail.com>
2018-08-15 19:14:17 +0200

    added command line argument parsing

--- src/corebuffer.cpp
+++ src/corebuffer.cpp
@@ -8,36 +8,31 @@
 #include "parser.h"
 #include "structurecheck.h"
 
+#include "args/args.hxx"
+
 using namespace std;
 
-void usage(const std::string &msg, const std::string &program)
+void usageError(const string &msg, const args::ArgumentParser &args)
 {
   cerr << msg << endl << endl;
-  cerr << "usage:" << endl;
-  cerr << "  " << program << " <input.cor> <output.h>" << endl;
+  cerr << args << endl;
 }
 
-void logError(const std::string &file, const FileError &pe)
+void logError(const string &file, const FileError &pe)
 {
   cerr << file << ":" << pe._state.line << ":" << pe._state.column << ": error: " << pe.what() << endl;
 }
 
-int main(int argc, char *argv[])
+int compile(const string &input, const string &output, const args::ArgumentParser &args)
 {
-  if (argc < 3)
-  {
-    usage("missing commad line argument(s).", argv[0]);
-    return 1;
-  }
-
-  std::ifstream t(argv[1]);
+  ifstream t(input);
   if (!t)
   {
-    usage(string("can not open input '") + argv[1] + "'.", argv[0]);
+    usageError("can not open input '" + input + "'.", args);
     return 2;
   }
 
-  std::string source((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+  string source((istreambuf_iterator<char>(t)), istreambuf_iterator<char>());
 
   Package p;
   try
@@ -45,29 +40,67 @@ int main(int argc, char *argv[])
     Parser(source, p).parse();
 
     auto errors = StructureCheck(p).check();
-    std::sort(errors.begin(), errors.end(),
-              [](const FileError &l, const FileError &r) { return l._state.pos < r._state.pos; });
+    sort(errors.begin(), errors.end(),
+         [](const FileError &l, const FileError &r) { return l._state.pos < r._state.pos; });
     if (!errors.empty())
     {
       for (const auto &pe : errors)
-        logError(argv[1], pe);
+        logError(input, pe);
       return 3;
     }
   }
   catch (const FileError &pe)
   {
-    logError(argv[1], pe);
+    logError(input, pe);
     return 3;
   }
 
-  std::ofstream o(argv[2]);
+  ofstream o(output);
   if (!o)
   {
-    usage(string("can not open output '") + argv[2] + "'.", argv[0]);
+    usageError("can not open output '" + output + "'.", args);
     return 4;
   }
 
   WriteCppCode(o, p);
 
   return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  args::ArgumentParser args("CoreBuffer compiler.");
+  args::HelpFlag help(args, "help", "Display this help menu", {'h', "help"});
+  args::Flag version(args, "version", "display the program version", {"version"});
+  args::Positional<string> input(args, "<input.cor>", "the CoreBuffer IDL descripting input file");
+  args::Positional<string> output(args, "<output.h>", "the c++ header output");
+
+  try
+  {
+    args.ParseCLI(argc, argv);
+  }
+  catch (args::Help)
+  {
+    cout << args;
+    return 0;
+  }
+  catch (args::ParseError e)
+  {
+    usageError(e.what(), args);
+    return 1;
+  }
+
+  if (version)
+  {
+    cout << "0.1" << endl;
+    return 0;
+  }
+
+  if (!input || !output)
+  {
+    usageError("missing commad line argument(s).", args);
+    return 1;
+  }
+
+  return compile(input.Get(), output.Get(), args);
 }
