89f8e25d77039448dc6350ceb45bb4352da1b30f
marco <spacesinmotion@gmail.com>
2018-08-16 11:29:29 +0200

    try fix build again -> provide unique version for appveyor

--- src/corebuffer.cpp
+++ src/corebuffer.cpp
@@ -13,9 +13,6 @@
 #ifndef COREBUFFER_VERSION
 #define COREBUFFER_VERSION "<undefined>"
 #endif
-#ifndef COREBUFFER_BUILD
-#define COREBUFFER_BUILD "<undefined>"
-#endif
 #ifndef COREBUFFER_BRANCH
 #define COREBUFFER_BRANCH "<undefined>"
 #endif
@@ -82,7 +79,7 @@ int main(int argc, char *argv[])
   args::ArgumentParser args(
       "CoreBuffer compiler. Code generation tool for convenient automatically generated binary serialization of "
       "complex data structures.",
-      __DATE__ " CoreBufferC " COREBUFFER_VERSION " build " COREBUFFER_BUILD " (" COREBUFFER_BRANCH ")");
+      __DATE__ " CoreBufferC " COREBUFFER_VERSION " (" COREBUFFER_BRANCH ")");
   args::HelpFlag help(args, "help", "Display this help menu", {'h', "help"});
   args::Flag version(args, "version", "display the program version", {"version"});
   args::Positional<string> input(args, "<input.cor>", "the CoreBuffer IDL descripting input file");
